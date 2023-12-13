ff416fd3de80a75768cabf987fb6ac25b76cbb6a
Anastasiya <MelAn_66@mail.ru>
2023-12-13 22:55:19 +0700

    First task commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+ let discriminant = Math.pow(b, 2) - (4 * a * c);
+  if (discriminant < 0) {
+    arr = [];
+  }
+  else if (discriminant == 0) {
+    arr[0] = (-b / (2 * a));
+  }
+  else if (discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
