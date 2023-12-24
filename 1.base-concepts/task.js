3debba435b93c75a80254610091ee7fe8e95ec08
Anastasiya <MelAn_66@mail.ru>
2023-12-24 23:07:33 +0700

    Выполнены задачи 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
- let discriminant = Math.pow(b, 2) - (4 * a * c);
-  if (discriminant < 0) {
+ let d = (Math.pow(b, 2)) - (4 * a * c);
+  if (d < 0) {
     arr = [];
   }
-  else if (discriminant == 0) {
+  else if (d == 0) {
     arr[0] = (-b / (2 * a));
   }
-  else if (discriminant > 0) {
+  else if (d > 0) {
     arr[0] = (-b + Math.sqrt(d)) / (2 * a);
     arr[1] = (-b - Math.sqrt(d)) / (2 * a);
   }
@@ -17,10 +17,9 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent /= 100;
-  P = percent / 12;
+  let P = (percent / 10) / 12;
   let S = amount - contribution;
-  let payment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
   let totalAmount = (payment * countMonths).toFixed(2);
   return totalAmount;
 }
