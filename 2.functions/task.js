cdf67ec39f617507cc39b3471e2a654762378aa3
Anastasiya <MelAn_66@mail.ru>
2023-12-27 22:36:33 +0700

    Добавлены тесты

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,14 +18,24 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: avg };
 }
 
+getArrayParams(-99, 99, 10); // { min: -99, max: 99, avg: 3.33 }
+getArrayParams(1, 2, 3, -100, 10); // { min: -100, max: 10, avg: -16.80 }
+getArrayParams(5); // { min: 5, max: 5, avg: 5 }
+
 function summElementsWorker(...arr) {
   let sum = 0;
+  if (arr.length === 0) {
+    return 0;
+
   for(let i = 0; i < arr.length; i++) {
     sum += arr[i]; 
   }
 
   return sum;
 }
+
+console.log(summElementsWorker()); // 0
+console.log(summElementsWorker(10, 10, 11, 20, 10)); // 61
 
 function differenceMaxMinWorker(...arr) {
   let min = arr[0];
