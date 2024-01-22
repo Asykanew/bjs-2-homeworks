fa03e1acdb2f53466287c942f641b25ae1b796cc
Anastasiya <MelAn_66@mail.ru>
2024-01-22 22:14:14 +0700

    Исправлены ошибки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,21 +10,18 @@ function Student(name, gender, age) {
   }
   
   Student.prototype.addMarks = function (...marks) {
-    Student.hasOwnProperty('marks')? marks.push(...marks): console.log('Нет оценок');
+    if(this.marks) {
+        this.marks.push(...marks)
+     } else {
+        console.log('Нет оценок');
   }
   
   Student.prototype.getAverage = function () {
-      if(!Student.hasOwnProperty('marks') || marks.lenght == 0) {
+      if(!this.marks || marks.lenght == 0) {
           return 0;
       }
       else {
-          marks.reduce((acc, item, index, arr) => {
-              acc += item;
-              if(index === arr.lenght - 1) {
-                  return acc / arr.lenght;
-              }
-              return acc;
-          }, 0);
+         return this.marks.reduce((sum, number) => sum + number, 0) / this.marks.length;
       }
   }
   
@@ -34,4 +31,17 @@ function Student(name, gender, age) {
       this.excluded = reason;
     
   }
+
+  let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
   
