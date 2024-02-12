26d7848bc3231b11622d2339ddf9708b406e0674
Anastasiya <MelAn_66@mail.ru>
2024-02-13 00:22:54 +0700

    Вторая задача

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,3 +14,42 @@ function validateCount(value) {
     return error;
   }
 }
+
+class Triangle {
+    constructor(firstSide, secondSide, thirdSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+    }
+    if(((firstSide + secondSide) < thirdSide) 
+    || ((firstSide + thirdSide) < secondSide) 
+|| ((secondSide + thirdSide) < firstSide)) {
+   throw new Error('Треугольник с такими сторонами не существует');
+}
+
+get perimeter() {
+    let p = (firstSide + secondSide + thirdSide);
+    return p;
+}
+get area() {
+    let s= Math.sqrt(0.5 * p * (0.5 * p - firstSide) * (0.5 * p - secondSide) * (0.5 * p - thirdSide)).toFixed(3);
+    return s;
+}
+}
+
+function getTriangle(firstSide, secondSide, thirdSide) {
+    try {
+        return new Triangle(firstSide, secondSide, thirdSide);
+    }
+    catch(error) {
+        return let eTriangle {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+
+        }
+    }
+}
