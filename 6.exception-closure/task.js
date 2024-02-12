e3daa7442a2790ae62f9807f4adf2192c8c5e3dd
Anastasiya <MelAn_66@mail.ru>
2024-02-12 22:32:44 +0700

    Первая функция по теме исключения

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,8 @@
-﻿
+﻿function parseCount(value) {
+    let count = Number.parseFloat(value);
+    if(count == NaN) {
+       throw new Error('Невалидное значение');
+    } else {
+        return count;
+    }
+}
